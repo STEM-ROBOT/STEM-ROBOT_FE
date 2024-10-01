819a0f6cec8e4609340d61bf85fba466091ee525
HoangChiDuong <duonghoangasd@gmail.com>
2024-10-01 20:32:27 +0700

    build web

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import GlobalStyles from "./GlobalStyles/index.jsx"; 
+
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
+  </StrictMode>
+);
