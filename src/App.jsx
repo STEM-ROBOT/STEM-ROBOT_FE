cfe6735cf2c1a5582a3643f82e126bc3b137b7ec
LDT1403 <thanhldse160337@fpt.edu.vn>
2024-10-06 11:25:05 +0700

    Update max-width home version02

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
-import AppRouter from './router/AppRouter'
+import Home from './pages/system-ui/layout/Home/Home'
+
 
 function App() {
   return (
     <>
-      <AppRouter />
+      <Home />
     </>
   )
 }
