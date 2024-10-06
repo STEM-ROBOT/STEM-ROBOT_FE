dc08463199c2ff974846184ffb4d99b9beabbd99
HoangChiDuong <duonghoangasd@gmail.com>
2024-10-06 11:31:04 +0700

    league version 0.2

--- src/router/SystemRouter.jsx
+++ src/router/SystemRouter.jsx
@@ -1,10 +1,9 @@
 import { Navigate } from "react-router-dom";
-import HomePage from "../pages/system-ui/layout/Home/Home";
-
+import Home from "../pages/system-ui/layout/Home/Home";
 
 const systemRoutes = [
   { path: '/', element:  <Navigate to="/home" /> },
-  { path: '/home', element: <HomePage /> },
+  { path: '/home', element: <Home /> },
  
 ];
 
