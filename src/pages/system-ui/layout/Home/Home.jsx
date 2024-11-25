dc147cb53508ba571b5779f15928ce73bb549c03
HoangChiDuong <duonghoangasd@gmail.com>
2024-11-25 15:55:02 +0700

    deploy version 0.5

--- src/pages/system-ui/layout/Home/Home.jsx
+++ src/pages/system-ui/layout/Home/Home.jsx
@@ -1,30 +1,32 @@
-
-import { useEffect, useState } from 'react';
-import Benefits from '../../component/Benefits/Benefits';
-import Footer from '../../component/Footer/Footer';
-import Header from '../../component/Header/Header';
-import TournamentFormats from '../../component/TournamentFormats/TournamentFormats';
-import TournamentSteps from '../../component/TournamentSteps/TournamentSteps';
-import './Home.css';
-import gearIcon from '/src/assets/images/service-reboticon.png';
-
+import { useEffect, useState } from "react";
+import Benefits from "../../component/Benefits/Benefits";
+import Footer from "../../component/Footer/Footer";
+import Header from "../../component/Header/Header";
+import TournamentFormats from "../../component/TournamentFormats/TournamentFormats";
+import TournamentSteps from "../../component/TournamentSteps/TournamentSteps";
+import "./Home.css";
+import gearIcon from "/src/assets/images/service-reboticon.png";
+import image from "/src/assets/images/banner-sideshape.png";
+import image2 from "/src/assets/images/banner-sideshape2.png";
+import image3 from "/src/assets/images/banner-image.png";
 const Home = () => {
   const [showHeader, setShowHeader] = useState(false);
 
   // Function to handle scroll events
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > window.innerHeight) { // Checks if scrolled more than 100vh
+      if (window.scrollY > window.innerHeight) {
+        // Checks if scrolled more than 100vh
         setShowHeader(true);
       } else {
         setShowHeader(false);
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -33,10 +35,10 @@ const Home = () => {
       <div className="banner_outer">
         <Header isVisible={showHeader} />
         <div className="overlay-container">
-          <img src="/src/assets/images/banner-sideshape.png" alt="Overlay" className="overlay-image" />
+          <img src={image} alt="Overlay" className="overlay-image" />
         </div>
         <div className="bottom-left-image-container">
-          <img src="/src/assets/images/banner-sideshape2.png" alt="Bottom Left" className="bottom-left-image" />
+          <img src={image2} alt="Bottom Left" className="bottom-left-image" />
         </div>
 
         <div className="content-wrapper">
@@ -49,13 +51,15 @@ const Home = () => {
               </span>
             </div>
             <div className="cta-buttons">
-              <button className="styled-button">
-                Tham gia
-              </button>
+              <button className="styled-button">Tham gia</button>
             </div>
           </div>
           <div className="bottom-right-image-container">
-            <img src="/src/assets/images/banner-image.png" alt="Bottom Right Image" className="banner_error" />
+            <img
+              src={image3}
+              alt="Bottom Right Image"
+              className="banner_error"
+            />
           </div>
         </div>
       </div>
